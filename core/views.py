d651735fe462d5a9453c32562d734030980066a4
mfelipegs <matheusg.dasilva7@gmail.com>
2022-06-11 14:04:18 -0300

    Added authentication function and login interface

--- core/views.py
+++ core/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render #, redirect
+from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
 #another way to redirect:
 #def index(request):
 #	return redirect('/agenda/')
 
+def login_user(request):
+	return render(request, 'login.html')
+
+def logout_user(request):
+	logout(request)
+	return redirect('/')
+	
+def submit_login(request):
+	if request.POST:
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect('/')
+		else:
+			messages.error(request, "Invalid username or password.")
+	return redirect('/')
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
 	user = request.user
 	evento = Evento.objects.filter(user=user)
