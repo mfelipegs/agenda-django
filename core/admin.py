82a4525f15380e6e813aded226d0b3d5dbbefc94
mfelipegs <matheusg.dasilva7@gmail.com>
2022-06-11 01:09:59 -0300

    v3

--- core/admin.py
+++ core/admin.py
@@ -4,5 +4,6 @@ from core.models import Evento
 
 class EventoAdmin(admin.ModelAdmin):
 	list_display = ('title', 'event_data', 'creation_data')
+	list_filter = ("user", "event_data",)
 
 admin.site.register(Evento, EventoAdmin)
